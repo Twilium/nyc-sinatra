a81404d9a18ce71f19f631ea8b41b606264bea2e
Emmanuel <rivera128@live.com>
2021-02-09 21:48:23 -0600

    commit tree 9a91346dd2163a10e65941bbb0849539a226d45c

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,44 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params[:landmark][:name])
+    erb :"figures/show"
+  end
+
+  get '/figure/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :"/figures/edit"
+  end
+
+  patch '/figures/:id' do 
+    ####### bug fix
+    if !params[:figure].keys.include?("figure_ids")
+      params[:figure]["figure_ids"] = []
+    end
+    #######
+
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+    if !params["figure"]["name"].empty?
+      @figure.landmarks << Figure.create(name: params["figure"]["name"])
+    end
+    redirect "figures/#{@figure.id}"
+  end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,43 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"/landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do 
+    ####### bug fix
+    if !params[:landmark].keys.include?("landmark_ids")
+      params[:landmark]["landmark_ids"] = []
+    end
+    #######
+
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
+  end
+
 end
